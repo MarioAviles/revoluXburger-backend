3c8f68e20ce49ca3ab1bd8fa88e1112d0c8852e6
MarioAviles <4150123@alu.murciaeduca.es>
2025-05-11 10:27:19 +0200

    Preparación para subirlo a render

--- install-java.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Installing Java 17..."
-sudo apt-get update
-sudo apt-get install -y openjdk-17-jdk
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
-export PATH=$JAVA_HOME/bin:$PATH
-java -version
