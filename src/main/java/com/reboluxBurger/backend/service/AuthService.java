44f7df3d0b24e5193bacb61fa274bc2e843ee56d
MarioAviles <4150123@alu.murciaeduca.es>
2025-05-23 22:17:28 +0200

    Cambios en el init para las url y correo de anonymous y cambio en el formato de la hora para los registros

--- src/main/java/com/reboluxBurger/backend/service/AuthService.java
+++ src/main/java/com/reboluxBurger/backend/service/AuthService.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -65,7 +66,7 @@ public class AuthService {
         String text = "Hola " + user.getUsername() + ",\n\n" +
                 "Te has registrado en Revoluxburger el día " +
                 LocalDateTime.now().toLocalDate() + " a las " +
-                LocalDateTime.now().toLocalTime() + ".\n\n" +
+                LocalDateTime.now().toLocalTime().format(DateTimeFormatter.ofPattern("HH:mm") + ".\n\n" +
                 "Gracias por unirte a nosotros.\n\nRebolux Burger 🍔";
 
         emailService.sendEmail(user.getEmail(), subject, text);
