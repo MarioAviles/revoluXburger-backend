08715bcfdf1cbdf64fab2daaad2d6c2b7a618b20
pablopalc <pablocryystalz24@gmail.com>
2025-05-26 11:36:15 +0200

    se añade un parametro de @NoArgs y se importa

--- src/main/java/com/reboluxBurger/backend/dto/MenuRequest.java
+++ src/main/java/com/reboluxBurger/backend/dto/MenuRequest.java
@@ -2,11 +2,14 @@ package com.reboluxBurger.backend.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 import java.math.BigDecimal;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class MenuRequest {
     private Long id;
     private String name;
